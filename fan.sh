0b8b60b335efaad048f490e84aaac76e35fcdfdb
scott-be <sb1975@gmail.com>
2015-02-22 16:42:06 -0500

    init

---
+++ fan.sh
@@ -0,0 +1,5 @@
+i=0; od -An -t x1 ~/message.bin | while read line; do \
+                   for c in $line; do \
+                       cmd=$(printf "i2cset -y 1 0x50 0x%x 0x$c b" $i); $cmd; ((i++));
+                   done;
+               done
